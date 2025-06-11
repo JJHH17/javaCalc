138a933873cb64d7ad4477e93de396e97b080cac
James Hatfield <james.hatfield17@icloud.com>
2025-06-11 11:23:45 +0100

    Added calculation code, loops until exit criteria/input is met

--- src/Main.java
+++ src/Main.java
@@ -7,32 +7,56 @@ public class Main {
         Scanner input = new Scanner(System.in);
 
         int inputNumber = 0; // Used for user input
-        String operator = "";
         int inputNumber2 = 0;
 
         String continueInput = ""; // Prompts if user wishes to continue
         int total = 0; // Used for answer
 
+        boolean flag = true;
+
         // Recurring loop for calculator, until exit criteria is met
-        while (true) {
+        while (flag) {
             System.out.println("Please enter a number: ");
             inputNumber = input.nextInt();
+            input.nextLine(); // Consumes new line
 
             System.out.println("Please enter an operator (+, -, /, *): ");
-            operator = input.nextLine();
+            String operator = input.nextLine();
 
             System.out.println("Please enter a second number: ");
             inputNumber2 = input.nextInt();
 
             // Running main calc checks
-
+            switch (operator) {
+                case "+":
+                    total = inputNumber + inputNumber2;
+                    System.out.println("Total: " + total);
+                    break;
+                case "-":
+                    total = inputNumber - inputNumber2;
+                    System.out.println("Total: " + total);
+                    break;
+                case "/":
+                    total = inputNumber / inputNumber2;
+                    System.out.println("Total: " + total);
+                    break;
+                case "*":
+                    total = inputNumber * inputNumber2;
+                    System.out.println("Total: " + total);
+                    break;
+                default:
+                    System.out.println("Please enter a valid operator");
+            }
 
             System.out.print("Do you wish to continue? ");
             continueInput = input.nextLine();
 
-            if (continueInput.equals("Exit")) {
-                break;
+            if (continueInput.equalsIgnoreCase("exit")) {
+                flag = false;
+            } else {
+                continue;
             }
         }
+        input.close();
     }
 }
